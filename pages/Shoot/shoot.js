e9baa522a16f3e2386965b15110f1bebf6f1a348
benny_lzb <benny_lzb@meten.com>
2018-12-13 14:40:55 +0800

    项目初始化以及目录搭建

---
+++ pages/Shoot/shoot.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+})
