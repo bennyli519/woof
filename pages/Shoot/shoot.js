e9ee461f9580eb6ca1b0899afdd30f247ab09f64
benny_lzb <benny_lzb@meten.com>
2018-12-14 09:54:59 +0800

    拍摄页样式及交互

--- pages/Shoot/shoot.js
+++ pages/Shoot/shoot.js
@@ -3,14 +3,22 @@
 const app = getApp()
 
 Page({
+  data:{
+    isTaken:false,
+  },
   takePhoto() {
     const ctx = wx.createCameraContext()
     ctx.takePhoto({
       quality: 'high',
       success: (res) => {
         this.setData({
+          isTaken:true
+        })
+        console.log(this.data.isTaken)
+        this.setData({
           src: res.tempImagePath
         })
+        console.log(this.data.src)
       }
     })
   },
